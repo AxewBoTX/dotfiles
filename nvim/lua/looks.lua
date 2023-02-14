04bf39ffc11e1ed8e05e782a2623b46cb88aed11
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-02-14 12:36:35 +0530

    commit

--- nvim/lua/looks.lua
+++ nvim/lua/looks.lua
@@ -2,4 +2,4 @@ vim.opt.termguicolors = true --Colors For The Terminal
 vim.opt.background = 'dark' --Background
 vim.opt.winblend = 0 --Transparency
 vim.opt.laststatus = 2 --Status Bar
-vim.cmd[[colorscheme monokai]] --ColorScheme
+vim.cmd[[colorscheme tokyonight]] --ColorScheme
--- zsh/zsh_config.zsh
+++ zsh/zsh_config.zsh
@@ -1,2 +1,3 @@
 alias ls="exa -all --icons"
 alias open="explorer.exe"
+export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_last"
