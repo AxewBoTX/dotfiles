293ce69ffd7898400c548f059bc6bdc8fa923a94
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-02-18 20:10:08 +0530

    commit

--- zsh/zsh_config.zsh
+++ zsh/zsh_config.zsh
@@ -2,3 +2,4 @@ alias ls="exa -all --icons"
 alias open="explorer.exe"
 export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_last"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs newline prompt_char)
+export PATH=$PATH:$HOME/.local/bin
