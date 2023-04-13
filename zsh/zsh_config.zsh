16d3b60026027f821ed2f3425e66e68e0dc49986
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-04-13 10:34:17 +0530

    commit

--- zsh/zsh_config.zsh
+++ zsh/zsh_config.zsh
@@ -3,3 +3,4 @@ alias tmux="tmux -u"
 export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_last"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs newline prompt_char)
 export PATH=$PATH:$HOME/.local/bin
+bindkey '^l' autosuggest-accept
