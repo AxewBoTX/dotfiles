fe4e1186059c56a4fa0ef8502f78a843355a39a2
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-04-13 19:41:38 +0530

    commit

--- zsh/zsh_config.zsh
+++ zsh/zsh_config.zsh
@@ -4,4 +4,3 @@ export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_last"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs newline prompt_char)
 export PATH=$PATH:$HOME/.local/bin
 bindkey '^l' autosuggest-accept
-bindkey '^i' clear-screen
