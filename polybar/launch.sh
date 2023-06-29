9267994bad1a5bc17b55e0d4f359fcad5d7a031c
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-06-29 07:02:29 +0530

    commit

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-# polybar-msg cmd quit
+If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch Polybar
 echo "---" | tee -a /tmp/polybar1.log 
-polybar topbar >&1 | tee -a /tmp/polybar1.log & disown
+polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
