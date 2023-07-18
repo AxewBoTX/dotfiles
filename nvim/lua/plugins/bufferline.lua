1316fe88dcae301af2e54fac62ff21c0e514fbae
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-07-18 19:39:47 +0530

    commit

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -72,7 +72,7 @@ end
 bufferline.setup({
 	options = {
 		mode = "tabs",
-		separator_style = "thin",
+		separator_style = "slant",
 		always_show_bufferline = false,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
