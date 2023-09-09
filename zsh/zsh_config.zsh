768445c21f3404cbde853a80c60bc56ba5da2341
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-09-09 17:58:25 +0530

    commit

--- zsh/zsh_config.zsh
+++ zsh/zsh_config.zsh
@@ -5,3 +5,5 @@ POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs newline prompt_char)
 export PATH=$PATH:$HOME/.local/bin
 bindkey '^l' autosuggest-accept
 bindkey '^K' clear-screen
+export  MESA_GLSL_VERSION_OVERRIDE=330
+export  MESA_GL_VERSION_OVERRIDE=3.3
