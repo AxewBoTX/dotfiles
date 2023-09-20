eda0bd2762a5b3fe93cc6d672c5642f071d7d9f3
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-09-20 12:55:56 +0530

    commit

--- zsh/zsh_config.zsh
+++ zsh/zsh_config.zsh
@@ -7,3 +7,15 @@ bindkey '^l' autosuggest-accept
 bindkey '^K' clear-screen
 export  MESA_GLSL_VERSION_OVERRIDE=330
 export  MESA_GL_VERSION_OVERRIDE=3.3
+
+#Ranger
+ranger-cd () {
+	tmp="$(mktemp)"
+	ranger --choosedir="$tmp" "$@"
+	if [ -f "$tmp" ]; then
+		dir="$(cat "$tmp")"
+		rm -f "$tmp"
+		[ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+	fi
+}
+alias ranger="ranger-cd"
