046c63947198ef05a6590064598b0aaea9431c80
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-10-21 11:55:58 +0530

    New Colorschemes

--- nvim/lua/axew/plugins/colorscheme.lua
+++ nvim/lua/axew/plugins/colorscheme.lua
@@ -1,9 +1,26 @@
 return {
 	{
 		"nyoom-engineering/oxocarbon.nvim",
+		lazy = true,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme oxocarbon]])
+			-- vim.cmd([[colorscheme oxocarbon]])
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- vim.cmd([[colorscheme kanagawa]])
+		end,
+	},
+	{
+		"kvrohit/mellow.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme mellow]])
 		end,
 	},
 }
