fa8c5f0e97aecebb3845299dc644bf60e9d151e0
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-10-21 18:48:53 +0530

    Rounded border

--- nvim/lua/axew/plugins/fterm.lua
+++ nvim/lua/axew/plugins/fterm.lua
@@ -4,4 +4,7 @@ return {
 		{ "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>', mode = "n" },
 		{ "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', mode = "t" },
 	},
+	opts = {
+		border = "rounded",
+	},
 }
