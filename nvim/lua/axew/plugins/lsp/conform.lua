f53e276a5a368ae489d8b7363cf7d9eb7523f176
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-10-23 20:26:44 +0530

    Replacing null-ls with conform.nvim

---
+++ nvim/lua/axew/plugins/lsp/conform.lua
@@ -0,0 +1,34 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { { "prettier", "prettierd" } },
+				typescript = { { "prettier", "prettierd" } },
+				javascriptreact = { { "prettier", "prettierd" } },
+				typescriptreact = { { "prettier", "prettierd" } },
+				svelte = { { "prettier", "prettierd" } },
+				css = { { "prettier", "prettierd" } },
+				html = { { "prettier", "prettierd" } },
+				json = { { "prettier", "prettierd" } },
+				yaml = { { "prettier", "prettierd" } },
+				markdown = { { "prettier", "prettierd" } },
+				graphql = { { "prettier", "prettierd" } },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "for", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+			})
+		end)
+	end,
+}
