401eb907e2d58eb37f38e94b076be5ced86202c0
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-10-27 10:05:22 +0530

    More colorschemes

--- nvim/lua/axew/plugins/colorscheme.lua
+++ nvim/lua/axew/plugins/colorscheme.lua
@@ -1,26 +1,50 @@
 return {
 	{
 		"nyoom-engineering/oxocarbon.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			-- vim.cmd([[colorscheme oxocarbon]])
+		end,
+	},
+	{
+		"Mofiqul/dracula.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme oxocarbon]])
+			vim.cmd([[colorscheme dracula]])
 		end,
 	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			-- vim.cmd([[colorscheme kanagawa]])
-		end,
-	},
-	{
-		"kvrohit/mellow.nvim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			-- vim.cmd([[colorscheme mellow]])
-		end,
-	},
+	-- {
+	-- 	"neanias/everforest-nvim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		-- vim.cmd([[colorscheme everforest]])
+	-- 	end,
+	-- },
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		-- vim.cmd([[colorscheme kanagawa]])
+	-- 	end,
+	-- },
+	-- {
+	-- 	"kvrohit/mellow.nvim",
+	-- 	lazy = true,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		-- vim.cmd([[colorscheme mellow]])
+	-- 	end,
+	-- },
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd([[colorscheme gruvbox]])
+	-- 	end,
+	-- },
 }
