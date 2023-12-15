3ddded5eee7be1b3d7d95fce3087eeb429270278
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-12-15 09:08:09 +0530

    Bufferline Style

--- nvim/lua/axew/plugins/bufferline.lua
+++ nvim/lua/axew/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
 		require("bufferline").setup({
 			options = {
 				mode = "tabs",
-				separator_style = "slant",
+				-- separator_style = "slant",
 				always_show_bufferline = false,
 				show_buffer_close_icons = false,
 				show_close_icon = false,
