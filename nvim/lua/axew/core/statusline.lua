1f4f371435a12eb5583a7c6d863a2406ac61f1ab
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-12-29 14:22:03 +0530

    Git repo status

--- nvim/lua/axew/core/statusline.lua
+++ nvim/lua/axew/core/statusline.lua
@@ -1,8 +1,13 @@
 -- Custom Statusline
---
-function GetGitBranch()
-	local branch = vim.fn.systemlist("git branch --show-current")[1]
-	return branch ~= nil and " " .. branch or ""
+
+function GetGitBranch() --Git repo status
+	local in_git_repo = vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null") == "true\n"
+	if in_git_repo then
+		local branch = vim.fn.systemlist("git branch --show-current")[1]
+		return branch ~= nil and " " .. branch or ""
+	else
+		return ""
+	end
 end
 
-vim.o.statusline = [[ %t %{v:lua.GetGitBranch()} %m %= [%l,%c] ]] --Statusline string
+vim.o.statusline = [[ %t %{v:lua.GetGitBranch()} %m %= [%l,%c] ]] --Statusline string
